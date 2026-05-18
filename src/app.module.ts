fb8c04f154c68243eba11fd0c81dfd6421f03e75
MrSevere <mrsevere483@gmail.com>
2026-05-18 08:10:17 +0300

    add Mongoose forRoot and forFeature configuration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BloggerPlatformModule } from './modules/blogger-platform/blogger-platform.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://MrSevere:qwertyadmin@ac-4suh2hg-shard-00-00.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-01.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-02.rtpcxjn.mongodb.net:27017/?ssl=true&replicaSet=atlas-sa4lbn-shard-0&authSource=admin&appName=Cluster0',
+    ),
+    BloggerPlatformModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Blogger Platform')
+    .setDescription('Documentation for blogger platform API')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
