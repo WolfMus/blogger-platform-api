cfb7f9bec43fd593260effdbba951557a96136b9
MrSevere <mrsevere483@gmail.com>
2026-05-21 15:58:30 +0300

    added name for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserAccountsModule } from './modules/user-accounts/user-accounts.module
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://MrSevere:qwertyadmin@ac-4suh2hg-shard-00-00.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-01.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-02.rtpcxjn.mongodb.net:27017/?ssl=true&replicaSet=atlas-sa4lbn-shard-0&authSource=admin&appName=Cluster0',
+      'mongodb://MrSevere:qwertyadmin@ac-4suh2hg-shard-00-00.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-01.rtpcxjn.mongodb.net:27017,ac-4suh2hg-shard-00-02.rtpcxjn.mongodb.net:27017/bloger-platform?ssl=true&replicaSet=atlas-sa4lbn-shard-0&authSource=admin&appName=Cluster0',
     ),
     BloggerPlatformModule,
     TestingModule,
