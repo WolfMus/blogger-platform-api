ebf59ef0f34f3201395edd203f5d765e43268db1
MrSevere <mrsevere483@gmail.com>
2026-05-25 16:22:49 +0300

    - added pagination for request dto

--- src/modules/blogger-platform/blogs/dto/create-blog.request.dto.ts
+++ src/modules/blogger-platform/blogs/dto/create-blog.request.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty, ApiSchema } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 @ApiSchema({
   name: 'CreateBlogRequestDto',
 })
 export class CreateBlogRequestDto {
   @ApiProperty({})
+  @Length(4, 15)
+  @IsString()
   name: string;
 
   @ApiProperty({})
+  @Length(1, 500)
   description: string;
 
   @ApiProperty({})
+  @IsEmail()
+  @Length(5, 100)
   websiteUrl: string;
 }
--- src/modules/blogger-platform/posts/dto/create-post.request.dto.ts
+++ src/modules/blogger-platform/posts/dto/create-post.request.dto.ts
@@ -1,26 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from 'class-validator';
 
 export class CreatePostForBlogRequestDto {
   @ApiProperty({})
+  @IsString()
+  @Length(3, 30)
   title: string;
 
   @ApiProperty({})
+  @Length(1, 100)
   shortDescription: string;
 
   @ApiProperty({})
+  @Length(1, 1000)
   content: string;
 }
 
 export class CreatePostRequestDto {
   @ApiProperty({})
+  @IsString()
+  @Length(3, 30)
   title: string;
 
   @ApiProperty({})
+  @Length(1, 100)
   shortDescription: string;
 
   @ApiProperty({})
+  @Length(1, 1000)
   content: string;
 
   @ApiProperty({})
+  @IsString()
   blogId: string;
 }
